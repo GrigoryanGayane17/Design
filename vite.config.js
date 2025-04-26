e7d30d698a16f3421741810e5bdb1e483dc871ff
gayanegrigoryan2008 <gayane.grigoryan2024@gmail.com>
2025-04-26 15:55:38 +0400

    section1,section2-->complated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Design-Solution-agency/", // <--- This is the repo name
   plugins: [react()],
 })
